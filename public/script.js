5747782e5334f58e23311e8b616fe97ab68b5e55
Lloyd Johnson <lloyd@theljohnson.com>
2024-12-08 17:46:17 -0800

    front end update

--- public/script.js
+++ public/script.js
@@ -3,15 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const fishContainer = document.getElementById("fish-container");
   
     scrapeLink.addEventListener("click", (event) => {
-      event.preventDefault(); // Prevent the default link action
+      //event.preventDefault(); // Prevent the default link action
   
       // Add animation class to fish container
       fishContainer.classList.add("animate");
   
       // Optional: Remove the animation class after a delay to allow reactivation
-      setTimeout(() => {
-        fishContainer.classList.remove("animate");
-      }, 5000); // Match the animation duration
-    });
+    //   setTimeout(() => {
+    //     fishContainer.classList.remove("animate");
+    //   }, 5000); // Match the animation duration
+     });
   });
   
