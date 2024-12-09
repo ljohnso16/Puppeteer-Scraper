bc6de5dbc9c83e47cebeafc09eb86800d32ae872
Lloyd Johnson <lloyd@theljohnson.com>
2024-12-08 19:19:53 -0800

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,24 +17,29 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://www.ana.co.jp/en/jp/international/");
 
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
 
     // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
+    //await page.type(".search-box__input", "automate beyond recorder");
+
+    const firstSpanSelector = "li[id^='be-overseas-tertiary-tab__item3']";
+    await page.waitForSelector(firstSpanSelector, { timeout: 20000 });
+    await page.click(firstSpanSelector);
 
     // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
+    //const searchResultSelector = ".search-box__link";
+    //await page.waitForSelector(searchResultSelector);
+    //await page.click(searchResultSelector);
 
     // Locate the full title with a unique string
     const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
+      "Flight Award Reservations"
     );
     const fullTitle = await textSelector.evaluate((el) => el.textContent);
+    await page.click(textSelector);
 
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
